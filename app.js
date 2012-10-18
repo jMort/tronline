edade3cc86e346d4ea34058035f0fd320170c08c
Joshua Morton <joshanator1995@gmail.com>
2012-10-18 20:01:35 +1100

    Made a simple socket.io chat

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-var http = require('http');
+/*var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('Hello from <a href="http://appfog.com">AppFog.com</a><br/>This is my message!');
-}).listen(process.env.VMC_APP_PORT || 1337, null);
+}).listen(process.env.VMC_APP_PORT || 1337, null);*/
+
+var port = process.env.VMC_APP_PORT || 8080;
+port = parseInt(port);
+console.log(port);
+var io = require('socket.io').listen(port);
+
+io.sockets.on('connection', function(socket) {
+	io.sockets.emit('receiveMessage', { message: 'NEW PERSON joined' });
+	socket.on('sendMessage', function(data) {
+		console.log(data);
+		io.sockets.emit('receiveMessage', data);
+	});
+	socket.on('disconnect', function() {
+		console.log('User disconnected');
+		io.sockets.emit('receiveMessage', { message: 'User disconnected' });
+	});
+});
