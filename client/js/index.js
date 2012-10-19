4c649d494f2277f5ce48794c4c1466472cf0d21b
Joshua Morton <joshanator1995@gmail.com>
2012-10-19 12:47:33 +1100

    Moved app.js to server folder and added client html to client folder

---
+++ client/js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	var socket = io.connect('http://transverse.ap01.aws.af.cm');
+	socket.on('receiveMessage', function(data) {
+		document.getElementById('messages').innerHTML += 'RECEIVED: '+data.message+'<br/>';
+	});
+	function sendMessage(message) {
+		socket.emit('sendMessage', { message: message });
+	}
+});
--- server/app.js
+++ server/app.js
@@ -7,15 +7,20 @@ http.createServer(function (req, res) {
 var port = process.env.VMC_APP_PORT || 8080;
 port = parseInt(port);
 console.log(port);
+
 var io = require('socket.io').listen(port);
 
+var numPlayers = 0;
+
 io.sockets.on('connection', function(socket) {
+	numPlayers++;
 	io.sockets.emit('receiveMessage', { message: 'NEW PERSON joined' });
 	socket.on('sendMessage', function(data) {
 		console.log(data);
 		io.sockets.emit('receiveMessage', data);
 	});
 	socket.on('disconnect', function() {
+		numPlayers--;
 		console.log('User disconnected');
 		io.sockets.emit('receiveMessage', { message: 'User disconnected' });
 	});
