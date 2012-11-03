290cd695a93fbb2adcafe5c21f6b76fbe14cc6e6
Joshua Morton <joshanator1995@gmail.com>
2012-11-04 09:57:29 +1100

    Tried running nodejs server on my own laptop

--- client/js/main.js
+++ client/js/main.js
@@ -1,5 +1,8 @@
+console.log('begin');
 $(document).ready(function() {
-	var socket = io.connect('http://transverse.ap01.aws.af.cm');
+	//var socket = io.connect('http://transverse.ap01.aws.af.cm');
+	var socket = io.connect('http://203.219.161.83:8080');
+	console.log('ready');
 	socket.on('receiveMessage', function(data) {
 		document.getElementById('messages').innerHTML += 'RECEIVED: '+data.message+'<br/>';
 	});
