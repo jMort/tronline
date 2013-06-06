20710b642922524cb1f77c9ba7ab0b0100ebdbd6
Joshua Morton <joshanator1995@gmail.com>
2013-06-06 11:42:50 +1000

    Fixed indentation of loginLogic.js and made it 2 spaces

--- public/js/login/loginLogic.js
+++ public/js/login/loginLogic.js
@@ -1,22 +1,22 @@
 define(function(require) {
   var login = function(socket, nickname, success, fail) {
-      socket.emit('checkLogin', nickname);
+    socket.emit('checkLogin', nickname);
 
-      // To avoid calling either callback more than once, we'll keep track when we call it.
-      var eitherCallbackCalled = false;
-      
-      socket.on('loginSuccessful', function() {
-        if (!eitherCallbackCalled) {
-          success();
-          eitherCallbackCalled = true;
-        }
-      });
-      socket.on('loginUnsuccessful', function() {
-        if (!eitherCallbackCalled) {
-          fail();
-          eitherCallbackCalled = true;
-        }
-      });
+    // To avoid calling either callback more than once, we'll keep track when we call it.
+    var eitherCallbackCalled = false;
+    
+    socket.on('loginSuccessful', function() {
+      if (!eitherCallbackCalled) {
+        success();
+        eitherCallbackCalled = true;
+      }
+    });
+    socket.on('loginUnsuccessful', function() {
+      if (!eitherCallbackCalled) {
+        fail();
+        eitherCallbackCalled = true;
+      }
+    });
   };
 
   return login;
