1b087931be5fa46900aae0059373a35f2809c887
Joshua Morton <joshanator1995@gmail.com>
2013-07-16 13:50:37 +1000

    Made the "updateDirection" event rewind a player by its ping and added tests for it

--- helper.js
+++ helper.js
@@ -1,4 +1,6 @@
-var Game = require('./public/js/game/Game');
+var Game      = require('./public/js/game/Game');
+var Player    = require('./public/js/game/Player');
+var constants = require('./public/js/game/constants');
 
 // Calculates the average ping divided by 2 (one-way trip)
 exports.calculateAveragePing = function(pingsArray) {
@@ -10,7 +12,7 @@ exports.calculateAveragePing = function(pingsArray) {
 };
 
 // Determines the game state X milliseconds ago from a set of snapshots
-exports.determineGameStateXMillisAgo = function(snapshots, millis, currentTime) {
+exports.determineGameStateXMillisAgoUsingSnapshot = function(snapshots, millis, currentTime) {
   var targetTime = currentTime - millis;
   var game = null;
   var timestamp = null;
@@ -21,4 +23,61 @@ exports.determineGameStateXMillisAgo = function(snapshots, millis, currentTime) 
     }
   }
   return { timestamp: timestamp, game: Game.clone(game) };
+};
+
+// Determines the player state X milliseconds ago
+exports.determinePlayerStateXMillisAgo = function(player, millis) {
+  // Clone the player first to avoid modifying the actual player
+  var newPlayer = Player.clone(player);
+
+  // Calculate how far the player must be moved backwards in time
+  var distance = parseInt(millis/(1000/constants.FPS) * newPlayer.speed);
+
+  // Keep removing the last node from the player's path, while the distance to rewind the player by
+  // is greater than the distance between the last two nodes
+  var lastDistance = newPlayer.lastDistance();
+  while (distance > lastDistance) {
+    // Remove the last node from the player's path
+    newPlayer.path.pop();
+    distance -= lastDistance;
+    lastDistance = newPlayer.lastDistance();
+
+    // Update the direction based on the last two nodes now
+    var lastIndex = newPlayer.path.length - 1;
+    if (newPlayer.path[lastIndex-1][0] == newPlayer.path[lastIndex][0]) {
+      if (newPlayer.path[lastIndex-1][1] < newPlayer.path[lastIndex][1])
+        newPlayer.direction = 'S';
+      else
+        newPlayer.direction = 'N';
+    } else {
+      if (newPlayer.path[lastIndex-1][0] < newPlayer.path[lastIndex][0])
+        newPlayer.direction = 'E';
+      else
+        newPlayer.direction = 'W;';
+    }
+  }
+
+  // Now if the player still needs to move back some more, move the last node backwards by this distance
+  if (distance > 0) {
+    var lastIndex = newPlayer.path.length - 1;
+    // If the last line the player made was vertical, move the distance in the y axis
+    // Otherwise it must be horizontal, so move the distance in the x axis
+    if (newPlayer.path[lastIndex-1][0] == newPlayer.path[lastIndex][0]) {
+      // If the second last node is higher up than the last node, move the last node upwards
+      // Otherwise move it downwards
+      if (newPlayer.path[lastIndex-1][1] < newPlayer.path[lastIndex][1])
+        newPlayer.path[lastIndex][1] -= distance;
+      else
+        newPlayer.path[lastIndex][1] += distance;
+    } else {
+      // If the second last node is further left than the last node, move the last node left
+      // Otherwise move it right
+      if (newPlayer.path[lastIndex-1][0] < newPlayer.path[lastIndex][0])
+        newPlayer.path[lastIndex][0] -= distance;
+      else
+        newPlayer.path[lastIndex][0] += distance;
+    }
+  }
+
+  return newPlayer;
 };
--- helperTest.js
+++ helperTest.js
@@ -1,6 +1,7 @@
 var helper = require('./helper');
-var calculateAveragePing         = helper.calculateAveragePing;
-var determineGameStateXMillisAgo = helper.determineGameStateXMillisAgo;
+var calculateAveragePing                      = helper.calculateAveragePing;
+var determineGameStateXMillisAgoUsingSnapshot = helper.determineGameStateXMillisAgoUsingSnapshot;
+var determinePlayerStateXMillisAgo            = helper.determinePlayerStateXMillisAgo;
 
 var Game = require('./public/js/game/Game');
 var Player = require('./public/js/game/Player');
@@ -19,7 +20,7 @@ exports.testCalculateAveragePing = function(test) {
   test.done();
 };
 
-exports.testDetermineGameStateXMillisAgo = function(test) {
+exports.testDetermineGameStateXMillisAgoUsingSnapshot = function(test) {
   var snapshots1 = {
     5: new Game(100, 100, [new Player('', 5, 5, 5, 'E', '')]),
     7: new Game(100, 100, [new Player('', 5, 5, 5, 'E', '')]),
@@ -27,13 +28,13 @@ exports.testDetermineGameStateXMillisAgo = function(test) {
     30: new Game(100, 100, [new Player('correct1', 5, 5, 5, 'E', '')])
   };
 
-  var game = determineGameStateXMillisAgo(snapshots1, 7, 31).game;
+  var game = determineGameStateXMillisAgoUsingSnapshot(snapshots1, 7, 31).game;
   test.equal(game.getPlayers()[0].nickname, 'correct1');
 
-  game = determineGameStateXMillisAgo(snapshots1, 11, 31).game;
+  game = determineGameStateXMillisAgoUsingSnapshot(snapshots1, 11, 31).game;
   test.equal(game.getPlayers()[0].nickname, 'correct2');
 
-  game = determineGameStateXMillisAgo(snapshots1, 21, 31).game;
+  game = determineGameStateXMillisAgoUsingSnapshot(snapshots1, 21, 31).game;
   test.equal(game.getPlayers()[0].nickname, 'correct2');
 
   var snapshots2 = {
@@ -44,13 +45,31 @@ exports.testDetermineGameStateXMillisAgo = function(test) {
     7000: new Game(100, 100, [new Player('correct3', 5, 5, 5, 'E', '')])
   };
 
-  game = determineGameStateXMillisAgo(snapshots2, 6000, 7000).game;
+  game = determineGameStateXMillisAgoUsingSnapshot(snapshots2, 6000, 7000).game;
   test.equal(game.getPlayers()[0].nickname, 'correct1');
 
-  game = determineGameStateXMillisAgo(snapshots2, 251, 7000).game;
+  game = determineGameStateXMillisAgoUsingSnapshot(snapshots2, 251, 7000).game;
   test.equal(game.getPlayers()[0].nickname, 'correct2');
 
-  game = determineGameStateXMillisAgo(snapshots2, 249, 7000).game;
+  game = determineGameStateXMillisAgoUsingSnapshot(snapshots2, 249, 7000).game;
   test.equal(game.getPlayers()[0].nickname, 'correct3');
+  test.done();
+};
+
+exports.testDeterminePlayerStateXMillisAgo = function(test) {
+  var player = new Player('', 5, 5, 5, 'E', '');
+  player.move();
+  player.move();
+  player.updateDirection('S');
+  var expectedPlayer = Player.clone(player);
+  player.move();
+  player.move();
+  player.updateDirection('E');
+  player.move();
+
+  // If the player is moved back 100ms, that is 100/(1000/30) = 3 frames
+  var newPlayer = determinePlayerStateXMillisAgo(player, 100);
+  test.deepEqual(newPlayer.path, expectedPlayer.path);
+  test.equal(newPlayer.getDirection(), expectedPlayer.getDirection());
   test.done();
 };
